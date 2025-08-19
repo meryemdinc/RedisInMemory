a6c275675d8a55c7c0dafec30da910619a3bdb74
meryemdinc <meryemdinc45@gmail.com>
2025-08-19 16:26:42 +0300

    Set cache priority to high Register PostEvictionCallback to track removal reason Store Product instance in memory cache

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers
@@ -21,9 +22,22 @@ namespace InMemoryApp.Web.Controllers
                 MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
                 options.AbsoluteExpiration=DateTime.Now.AddMinutes(1);
                 options.SlidingExpiration = TimeSpan.FromSeconds(10);
+                options.Priority = CacheItemPriority.High;
 
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                _memoryCache.Set("callback",$"{key}->{value} => sebep = {reason}");
+                });
                 _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
             }
+
+            Product p= new Product
+            {
+                Id = 1,
+                Name = "Kalem",
+                Price = 10
+            };
+            _memoryCache.Set<Product>("product:1",p);
 
             return View();
         }
@@ -36,7 +50,11 @@ namespace InMemoryApp.Web.Controllers
                   return DateTime.Now.ToString();
               });*/
             _memoryCache.TryGetValue("zaman", out string zamancache);
+            _memoryCache.TryGetValue("callback", out string callback);
+
             ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
             return View();
         }
     }
