1eee8bf2bcb5167f650666ea4024f0a4d1ac77e0
meryemdinc <meryemdinc45@gmail.com>
2025-08-19 12:30:33 +0300

    Implement absolute and sliding expiration caching strategies

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -18,8 +18,11 @@ namespace InMemoryApp.Web.Controllers
         {
             if (!_memoryCache.TryGetValue("zaman", out string zamancache))
             {
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration=DateTime.Now.AddMinutes(1);
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
 
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString(),options);
             }
 
             return View();
@@ -27,12 +30,13 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Show()
         {
-          /*  _memoryCache.GetOrCreate<string>("zaman", entry =>
-            {
-               
-                return DateTime.Now.ToString();
-            });*/
-            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            /*  _memoryCache.GetOrCreate<string>("zaman", entry =>
+              {
+
+                  return DateTime.Now.ToString();
+              });*/
+            _memoryCache.TryGetValue("zaman", out string zamancache);
+            ViewBag.zaman = zamancache;
             return View();
         }
     }
