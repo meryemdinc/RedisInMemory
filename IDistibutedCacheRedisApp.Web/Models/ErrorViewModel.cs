1d47a6b5ac3d32c062d0762d6a84181d58b11275
meryemdinc <meryemdinc45@gmail.com>
2025-08-23 19:21:20 +0300

    feat: add StackExchange.Redis package and initial project setup

---
+++ IDistibutedCacheRedisApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IDistibutedCacheRedisApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
