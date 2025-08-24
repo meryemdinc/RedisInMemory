ba24a3fc87c51b8df7efdf566999ce79e472c744
meryemdinc <meryemdinc45@gmail.com>
2025-08-24 20:46:40 +0300

    feat: integrate RedisService via DI and add initial StringTypeController

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    //burada redis service i kullanmak istiyorsak buranın contructorına DI olarak geçmek lazım
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+        public IActionResult Index()
+        {
+            var db = _redisService.GetDb(0);
+            db.StringSet("name", "Meryem Dinç");
+            db.StringSet("visitors", 100);
+
+
+            return View();
+        }
+    }
+}
