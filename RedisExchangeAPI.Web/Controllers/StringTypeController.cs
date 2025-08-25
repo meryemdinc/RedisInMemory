5e60afab49e7f3853612f9df5aaf36fdc82a8179
meryemdinc <meryemdinc45@gmail.com>
2025-08-25 20:02:03 +0300

    feat(controller): implemented Redis string operations

--- RedisExchangeAPI.Web/Controllers/StringTypeController.cs
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+using StackExchange.Redis.KeyspaceIsolation;
 
 namespace RedisExchangeAPI.Web.Controllers
 {
@@ -7,16 +9,38 @@ namespace RedisExchangeAPI.Web.Controllers
     public class StringTypeController : Controller
     {
         private readonly RedisService _redisService;
+        private readonly IDatabase db;
         public StringTypeController(RedisService redisService)
         {
             _redisService = redisService;
+            db = _redisService.GetDb(0);
         }
         public IActionResult Index()
         {
-            var db = _redisService.GetDb(0);
+
             db.StringSet("name", "Meryem Dinç");
             db.StringSet("visitors", 100);
 
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            var value= db.StringGet("name");
+            if (value.HasValue)
+            {
+                ViewBag.value=value.ToString();
+            }
+            db.StringIncrement("visitors", 1);
+             var value2 = db.StringDecrementAsync("visitors", 10).Result;
+
+                ViewBag.value2 = value2.ToString();
+         
+            var value3=db.StringGetRange("name", 0, 3);
+            ViewBag.value3 = value3.ToString();
+
+            var value4=db.StringLength("name");
+            ViewBag.value4 = value4.ToString(); 
 
             return View();
         }
